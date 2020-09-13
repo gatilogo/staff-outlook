19f85454fd1038e8019c626a9869cf859a9afbb7
Trishia Gatilogo <gatilogo@ualberta.ca>
2020-09-12 21:23:15 -0600

    adding staff outlook website

---
+++ scripts/animations.js
@@ -0,0 +1,8 @@
+// Scroll to view on navigation menu click
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
