e9e32c5bf0cd35a94fb6a8a91bd0aaa9edca6d44
Trishia Gatilogo <gatilogo@ualberta.ca>
2020-09-16 22:11:08 -0600

    fancy hover animation

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,8 +1,15 @@
-// Scroll to view on navigation menu click
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+$(document).ready(function () {
+    // Scroll to view on navigation menu click
+    $('a[href ^= "#"]').on('click', function (event) {
+        event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 2000);
+    });
+
+    // Hamburger menu button animation
+    $('.menu-btn').on('click', function () {
+        $(this).toggleClass('change');
+    });
 });
